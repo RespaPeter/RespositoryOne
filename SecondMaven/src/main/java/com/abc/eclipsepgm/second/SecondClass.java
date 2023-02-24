9c309b75ce6b741c4ec1755abc01f5b8cc6d42f7
PRAGAL <PRAGAL@DESKTOP-CRHJTDS>
2023-02-24 10:23:48 +0400

    commited

---
+++ SecondMaven/src/main/java/com/abc/eclipsepgm/second/SecondClass.java
@@ -0,0 +1,11 @@
+package com.abc.eclipsepgm.second;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HI");
+
+	}
+
+}
